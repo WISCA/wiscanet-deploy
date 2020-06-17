e19d7f1628d7e7bc7c94a3244b558dfb47e96c6d
Jacob Holtom <jacob@holtom.me>
2020-06-17 14:41:32 -0700

    Format code

--- run/usr/mat/UMAC_sdrn/helperFunc/mmseEqualizer.m
+++ run/usr/mat/UMAC_sdrn/helperFunc/mmseEqualizer.m
@@ -1,12 +1,12 @@
 function  [s_hat, g] = mmseEqualizer(x, xtrn, s, Kg, d)
 %Input:  x: Output of channel to a (training) data sequence a
 %        s: Message data, Kg=# of equalizer taps
-%        d: Expected channel & equalizer delay 
-%Output: 
+%        d: Expected channel & equalizer delay
+%Output:
 %        g: Equalizer coefficients
-% L = length(x); 
-% phi_x = xcorr(x, 'unbiased'); 
-% phi_sx = xcorr(s, x, 'unbiased');   
+% L = length(x);
+% phi_x = xcorr(x, 'unbiased');
+% phi_sx = xcorr(s, x, 'unbiased');
 % Rxx = zeros(Kg,Kg);
 % rsx = zeros(Kg,1);
 % d = 0;
@@ -15,7 +15,7 @@ function  [s_hat, g] = mmseEqualizer(x, xtrn, s, Kg, d)
 %    rsx(i) = phi_sx(L+i-1-d);          % Eq.(6.2.11)
 % end
 
-    % Compute Autocovariance estimate 
+    % Compute Autocovariance estimate
     tmp = xcorr(xtrn, Kg);
     Rxx = toeplitz(tmp(Kg+1:end-1));
 
@@ -28,5 +28,5 @@ function  [s_hat, g] = mmseEqualizer(x, xtrn, s, Kg, d)
 
     % Output equalized signal
     s_hat = filter(g, 1, x);
-    
+
 end
--- run/usr/mat/UMAC_sdrn/usrpFunc/usrpQPSKNode.m
+++ run/usr/mat/UMAC_sdrn/usrpFunc/usrpQPSKNode.m
@@ -1,37 +1,26 @@
 function eof = usrpQPSKNode(sys_start_time)
 
-    %%
+    %
     % Add relevant paths
     %
     addpath(genpath('../../lib'));
     addpath(genpath('../'));
-    
-    %%
-    % Initialize radio
-    %
 
     % Load params file
-      load('../paramsFiles/params17Sep2018.mat'); % Commented for
-      %debugging
-      %load('paramsFiles/params17Sep2018.mat');
-   
-      [usrpSettings, usrpRadio] = initUSRPRadio(commsParams);
-   
-    % '2017-05-11 00:00:00'
-    if 0
-       startTime = setStartTime();%#ok
-    else 
-       startTime = sys_start_time; %time from manager
-    end%
-    
-    %%
+    load('../paramsFiles/params17Sep2018.mat');
+    % Initialize Radio
+    [usrpSettings, usrpRadio] = initUSRPRadio(commsParams);
+    % Start time from control system
+    startTime = sys_start_time;
+
+    %
     % Constant variable definitions
     %
     lId = usrpRadio.logicalId();
     lIdStr = num2str(lId);
-   
+
     % Misc Initializations
-    
+
     sampFreq       = commsParams.sampFreq;
     upsampFactor   = commsParams.upsampFactor;
     modOrder       = commsParams.modOrder;
@@ -44,86 +33,78 @@ function eof = usrpQPSKNode(sys_start_time)
     nUsrs          = commsParams.nUsrs;
     nUsrBits	   = commsParams.nUsrBits;
     nMsgBits	   = commsParams.nMsgBits;
-    msgBits        = commsParams.msgBits;    
+    msgBits        = commsParams.msgBits;
     initMsgBits    = msgBits;
     modMsgSyms     = ((nMsgBits+nUsrBits)/bitsPerSym)*(spreadVal/bitsPerSym);
     modMsgSize     = modMsgSyms*upsampFactor;
     msgHat         = zeros(nMsgBits+nUsrBits,1);
     encdBits       = msgHat;
-    
+
     buffSize       = usrpSettings.buff_samps;
     buffEnd        = 2*buffSize;
-    buffErrs       = zeros(nCycles,1);    
+    buffErrs       = zeros(nCycles,1);
     bitErrs        = zeros(nCycles,1);
     txFlag         = false;
     procTime       = usrpSettings.proc_time;
-    
-    
-    %%
+
+
+    %
     % Variable per User
-    % 
+    %
 
     % Message variables
     trnSyms        = msgCodeBook.trnSymbols;
     spreadIdx      = msgCodeBook.spreadIdx;
     permSeqIntrlvr = msgCodeBook.permSeqIntrlvr;
-        
-    % Allocate data buffer 
+
+    % Allocate data buffer
     txBuff  = zeros(buffEnd, 1);
     rxBuff  = zeros(buffEnd, 1);
     rxWav = zeros(buffSize, 1);
-    
-    %%
+
+    %
     %   Transmitter Initialization
     %
-    
-    [modulator, txFilter] = initCommsTXMod(upsampFactor, modOrder);    
+    [modulator, txFilter] = initCommsTXMod(upsampFactor, modOrder);
     [intrlvr] = initCommsTXErrorCorrection(permSeqIntrlvr);
-    
-    %%
-    %   Receiver InitializationQPSKT
-    %
-                %[rxMsg, rxTrn, rxNzeTrn] = commsRXSync(...
-              %rxFiltWav, trnSyms, modMsgSize, commsParams); %#ok
 
-    % Initialization for synchronization and acquisition OK
+    %
+    %   Receiver Initialization
+    %
+    % Initialization for synchronization and acquisition
     [freqEstimator, freqCompensator, rxFilter] = initCommsRXSync(...
         modOrder, upsampFactor, sampFreq);
-    
+
     [deIntrlvr] = initCommsRXErrorCorrection(permSeqIntrlvr);
-    [demodulator] = initCommsRXDemod(modOrder);    
+    [demodulator] = initCommsRXDemod(modOrder);
     nextUsrId = randi([2 nUsrs]);
+
     % Initialize message counter
     count = 1;
-    
-    %%    addpath(genpath('../'));
-    %Transmit/Receive Loop
-    
-    
 
+    %Transmit/Receive Loop
     for nn = 1:nCycles
 
-        %Transmit
-        if (lId == 1 && nn == 1) || (txFlag == true) 
-            
+        if (lId == 1 && nn == 1) || (txFlag == true) % Transmit
+
             fprintf('\nFrame Number: %i\n', count)
             fprintf('Transmitting to User %i\n\n', nextUsrId)
-            
-            % Generate new message bits with updated nxtUsr  
+
+            % Generate new message bits with updated nxtUsr
             [encdBits, fullMsgBits] = genMsgBits(...
-                intrlvr, msgBits, nUsrBits, nextUsrId);  
+                intrlvr, msgBits, nUsrBits, nextUsrId);
 
             % Generate waveform packets
             commsWav = generateWaveform(...
             	trnSyms, sfxSyms, encdBits, ...
                 spreadIdx, spreadVal, modulator, txFilter);
                 nextUsrId = randi([2 nUsrs]);
-            
+
 	        commsWav = commsWav/max(abs(commsWav));
-            
+
             % save last Tx Wave
-             fileName = ['usrpQPSKNode_txWav_',lIdStr, '.mat'];
-             save(fullfile('~/Data', fileName), 'commsWav');
+            fileName = ['usrpQPSKNode_txWav_',lIdStr, '.mat'];
+            save(fullfile('~/Data', fileName), 'commsWav');
 
             % Transmit waveform packets
             len = numel(commsWav);
@@ -131,32 +112,26 @@ function eof = usrpQPSKNode(sys_start_time)
             txBuff(2:2:2*len) = (imag(commsWav));
 
             % Transmit waveform packets
-            usrpRadio.tx_usrp(startTime, txBuff);        
- 	
-	    txFlag = false;		
- 
-	    count = count+1;
- 
-        else
-            		   
-	    fprintf('\n\nFrame Number %i\n', count)
-        fprintf('Receiving\n')
+            usrpRadio.tx_usrp(startTime, txBuff);
 
-            % Receive 
-            %
+    	    txFlag = false;
+    	    count = count+1;
+        else % Receive
+	        fprintf('\n\nFrame Number %i\n', count)
+            fprintf('Receiving\n')
 
+            % Receive
             rxBuff = (usrpRadio.rx_usrp(startTime)).';
-            %[rxBuff, buffErrs(nn)] = buffErrorCorrect(rxBuff, buffEnd);              
 
             % Convert to complex format
             rxWav = complex(rxBuff(1:2:end),rxBuff(2:2:end));
-            
-              % save last Rx Wave
-              fileName = ['usrpQPSKsys_start_timeNode_rxWav_',lIdStr, '.mat'];
-              save(fullfile('~/Data', fileName), 'rxWav');
-            
-             % Generate spread sequence
-             spreadSeq = genModSpreadSeq(...
+
+            % save last Rx Wave
+            fileName = ['usrpQPSKsys_start_timeNode_rxWav_',lIdStr, '.mat'];
+            save(fullfile('~/Data', fileName), 'rxWav');
+
+            % Generate spread sequence
+            spreadSeq = genModSpreadSeq(...
                          spreadVal, spreadIdx, modulator);
 
             % Compensate for frequency offset and apply matched filter
@@ -177,62 +152,51 @@ function eof = usrpQPSKNode(sys_start_time)
             % Msg Decode
             msgHat = commsRXDecode(rxFullMsgBits(:), deIntrlvr);
 
-            %%
-            %Extract User Data
+            % Extract User Data
             % Convert binary to decimal values
-            
             rxMsgBits = msgHat(1:nMsgBits);
             rxUsrBits = msgHat(nMsgBits+1:end);
-            rxUsrId = binMsgDecode(rxUsrBits.');    % Debugging-This feels like the culprit! The operands are not integers after running th demo for a while 
+            rxUsrId = binMsgDecode(rxUsrBits.');    % Debugging-This feels like the culprit! The operands are not integers after running th demo for a while
 
             [bitErrs, BER] = biterr(rxMsgBits, initMsgBits);
-           %fprintf('Bit Errors = %i\n', bitErrs)
-	    fprintf('Bit Error Rate = %i\n',BER)
-            
+	        fprintf('Bit Error Rate = %i\n',BER)
+
             % set Tx flag
-            
             [txFlag, nextUsrId] = setTxFlag(lId, rxUsrId, nUsrs);
 
          if txFlag == true
 			fprintf('TX FLAG = HIGH\nNEXT USER = %i\n', nextUsrId)
 		 	msgBits = rxMsgBits;
-            
+
 		 else
 			fprintf('TX FLAG = LOW\nIDLE\n')
 		 end
-                       
+
             count = count+1;
 
-        end 
+        end
 
-        startTime = startTime+procTime; 
-    end 
+        startTime = startTime+procTime;
+    end
 
-                
     % Release allocated objects
     releaseObjs(modulator, demodulator, txFilter, ...
         rxFilter, intrlvr, deIntrlvr, freqEstimator, ...
         freqCompensator);
-       
+
     dateStr  = datestr(now, 'mmddyyyy_HHMMSS');
     fileName = ['usrpQPSKNode', dateStr, '.mat'];
-    save(fullfile('../Data', fileName), 'rxWav');
-         
-        
-    % Stop server 
+    save(fullfile('~/Data', fileName), 'rxWav');
+
+    % Stop radio server
     usrpRadio.terminate_usrp();
-    
+
     % Set end of function flag
     eof = true;
-
-    success = true;
-
 end
 
 function [ usrpSettings, usrpRadio ] = initUSRPRadio(p)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-        
+
     usrpSettings.bw             = 100e6;        %bw
     usrpSettings.sample_rate    = 20e6;         %rate
     usrpSettings.rx_gain        = 40;           %rx_gain
@@ -248,33 +212,13 @@ function [ usrpSettings, usrpRadio ] = initUSRPRadio(p)
     usrpSettings.setup_time     = 2;          %setup_time2
     usrpSettings.proc_time 	= 4;            %MATLAB processing time (seconds)
 
-    
+
     usrpRadio = local_usrp;
-    
+
 	usrpRadio  =   usrpRadio.set_usrp(...
-        usrpSettings.type, usrpSettings.ant, usrpSettings.subdev, ... 
+        usrpSettings.type, usrpSettings.ant, usrpSettings.subdev, ...
         usrpSettings.ref, usrpSettings.wirefmt, usrpSettings.buff_samps,...
         usrpSettings.sample_rate, usrpSettings.freq, usrpSettings.rx_gain, ...
         usrpSettings.tx_gain, usrpSettings.bw, usrpSettings.setup_time);
 
 end
-
-function posixTime = setStartTime(dateTimeStr) %#ok
-
-    if ~exist('dateTimeStr','var') || isempty(dateTimeStr)
-        
-        p = posixtime(datetime('now', 'Timezone', 'America/Phoenix'));
-        posixTime = double(uint64(p)+5);       
-        
-    else
-        
-        p = posixtime(datetime(...
-            dateTimeStr, 'InputFormat', 'yyy-MM-dd HH:mm:ss', ...
-            'Timezone','America/Phoenix'));
-        
-        posixTime = double(uint64(p));
-        
-    end
-
-end
-
